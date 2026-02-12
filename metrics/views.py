e69ea939f27bb75451e42d7f3fe806e5626547f1
Ankita Jha <ankita6mar@gmail.com>
2026-02-12 14:42:31 +0530

    Created views in Metrics

--- metrics/views.py
+++ metrics/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .services import (
+    most_unstable_service,
+    calculate_mttr,
+    deployment_reliability,
+)
+
+class MetricsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        unstable = most_unstable_service()
+        mttr = calculate_mttr()
+        reliability = deployment_reliability().values(
+            "name", "failure_rate"
+        )
+
+        return Response({
+            "most_unstable_service": unstable.name if unstable else None,
+            "average_mttr": mttr["avg_mttr"],
+            "deployment_reliability": list(reliability),
+        })
